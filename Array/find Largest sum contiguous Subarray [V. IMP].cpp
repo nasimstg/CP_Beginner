40d70f102307ae51da95ebfcb319a802db071536
Mohammed Nasim <75156273+nasimstg@users.noreply.github.com>
2023-07-27 23:53:58 +0600

    Create find Largest sum contiguous Subarray [V. IMP].cpp

---
+++ Array/find Largest sum contiguous Subarray [V. IMP].cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        long long maxh = 0, maxf = arr[0];
+        
+        for(int i=0;i<n;i++) 
+            {
+                maxh+=arr[i];
+                if(maxf<maxh)maxf=maxh;
+                if(maxh<0)maxh=0;
+    
+            }
+            //returning the result.
+            return maxf;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+
+// } Driver Code Ends
